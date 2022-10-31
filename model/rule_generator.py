6e0bb146ddf6662be6beac9627b09cac3c6daee1
Kecskeméti Károly <kecskemeti.karoly42@gmail.com>
2022-10-31 03:18:19 +0100

    Create rule_generator.py

---
+++ model/rule_generator.py
@@ -0,0 +1,64 @@
+from enum import Enum
+from typing import Dict, List
+
+
+class Table(Enum):
+    FILTER = 1
+    NAT = 2
+    MANGLE = 3
+
+
+class Chain(Enum):
+    INPUT = 1
+    OUTPUT = 2
+    FORWARD = 3
+    PREROUTING = 4
+    POSTROUTING = 5
+
+class RuleComponent:
+    def __init__(self, raw_form: string):
+        parsed_form = self.parse_raw_form(raw_form)
+        self.string_form = parsed_form["string_form"]
+        self.explanation = parsed_form["explanation"]
+        if "value" in parsed_form:
+            self.value = parsed_form["value"]
+        else:
+            self.value = None
+    
+    def parse_raw_form(self, raw_form: string) -> Dict[string, string]:
+        pass
+    
+    def has_value(self) -> bool:
+        return self.value is not None
+
+
+class Rule:
+    def __init__(self, table: Table, chain: Chain):
+        self.table = table
+        self.chain = chain
+        self.components = []
+    
+    def __init__(self, raw_form):
+        parsed_form = self.parse_raw_form(raw_form)
+        
+    def parse_raw_form(self, raw_form) -> List[Dist[string, string]]:
+        pass
+        
+    def check_total_correctness(self) -> bool:
+        pass
+
+    def check_partial_correctness(self) -> bool:
+        pass
+    
+    def delete_element(self,id) -> bool:
+        pass
+    
+    def get_possible_elements(self) -> List:
+        pass
+
+    def set_value(self) -> bool:
+        pass
+    
+    def get_elements(self):
+        pass
+    
