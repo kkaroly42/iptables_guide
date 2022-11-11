aa49fc15bb6555c6ede4e6072defc0fbba6ee3b8
Kecskeméti Károly <kecskemeti.karoly42@gmail.com>
2022-11-11 19:18:15 +0100

    Add extended, almost complete rule generator logic

---
+++ model/parser_entries.py
@@ -0,0 +1,43 @@
+
+start_strs = {
+    "iptables": {
+        "str_form": "iptables",
+        "explanation": "Ezzel a kapcsolóval azt jelöljük meg, hogy a NAT(Network Address Translation) táblára fog vonatkozni az aktuális szabály.",
+        "value": "NAT",
+    }
+}
+
+possible_tables = {
+    "-t NAT": {
+        "str_form": "-t NAT",
+        "explanation": "Ezzel a kapcsolóval azt jelöljük meg, hogy a NAT(Network Address Translation) táblára fog vonatkozni az aktuális szabály.",
+        "value": "NAT",
+    },
+    "-t FILTER": {
+        "str_form": "-t FILTER",
+        "explanation": "Ezzel a kapcsolóval azt jelöljük meg, hogy a FILTER táblára fog vonatkozni az aktuális szabály.",
+        "value": "FILTER",
+    },
+}
+
+possible_commands = {
+    "-A": {
+        "str_form": "-A",
+        "explanation": "Az append kapcsoló a lánc végére fűzi be az új szabályt.",
+    },
+    "-I": {
+        "str_form": "-I",
+        "explanation": "Az insert kapcsoló a láncon belül megadott helyre szúrja be az új szabályt.",
+    },
+}
+
+possible_chains = {
+    "": {
+        "str_form": "-A",
+        "explanation": "Az append kapcsoló a lánc végére fűzi be az új szabályt.",
+    },
+    "-I": {
+        "str_form": "-I",
+        "explanation": "Az insert kapcsoló a láncon belül megadott helyre szúrja be az új szabályt.",
+    },
+}
--- model/rule_generator.py
+++ model/rule_generator.py
@@ -1,64 +1,153 @@
 from enum import Enum
-from typing import Dict, List
+from typing import Dict, List, Optional, Tuple, Union
+from abc import abstractmethod
+import parser_entries
 
 
-class Table(Enum):
-    FILTER = 1
-    NAT = 2
-    MANGLE = 3
-
-
-class Chain(Enum):
-    INPUT = 1
-    OUTPUT = 2
-    FORWARD = 3
-    PREROUTING = 4
-    POSTROUTING = 5
-
-class RuleComponent:
-    def __init__(self, raw_form: string):
-        parsed_form = self.parse_raw_form(raw_form)
-        self.string_form = parsed_form["string_form"]
-        self.explanation = parsed_form["explanation"]
-        if "value" in parsed_form:
-            self.value = parsed_form["value"]
-        else:
-            self.value = None
-    
-    def parse_raw_form(self, raw_form: string) -> Dict[string, string]:
+class SignatureComponent:
+    @abstractmethod
+    def find_fit(self, substr: List[str]) -> Optional[Tuple[Dict[str, str], List[str]]]:
         pass
-    
-    def has_value(self) -> bool:
-        return self.value is not None
+
+
+class StartComponent(SignatureComponent):
+    def __init__(self, start_strs):
+        self.start_strs = start_strs
+
+    def find_fit(self, substr: List[str]) -> Optional[Tuple[Dict[str, str], List[str]]]:
+        to_find = substr[0]
+        if to_find in self.start_strs:
+            return self.start_strs[to_find], substr[1:]
+        else:
+            return None
+
+
+class TableComponent(SignatureComponent):
+    def __init__(self, tables: Dict[str, Dict[str, str]]):
+        self.possible_tables = tables
+
+    def find_fit(self, substr: List[str]) -> Optional[Tuple[Dict[str, str], List[str]]]:
+        to_find = " ".join(substr[:2])
+        if to_find in self.possible_tables:
+            return self.possible_tables[to_find], substr[2:]
+        else:
+            return None
+
+
+class RuleSpecification:
+    def __init__(self, spec_components):
+        self.possible_components = spec_components
+
+    def find_fit(
+        self, substr: List[str]
+    ) -> Optional[List[Tuple[Dict[str, str], List[str]]]]:
+        specs = []
+        i = 0
+        while i < len(self.possible_components) and substr:
+            result = self.possible_components[i].find_fit(substr)
+            if result:
+                specs.append(result[0])
+                substr = result[1]
+        return specs if len(specs) > 0 else None
+
+
+class CommandComponent(SignatureComponent):
+    def __init__(self, commands):
+        self.possible_commands = commands
+
+    def find_fit(self, substr: List[str]) -> Optional[Tuple[Dict[str, str], List[str]]]:
+        to_find = substr[0]
+        if to_find in self.possible_commands:
+            return self.possible_commands[to_find], substr[1:]
+        else:
+            return None
+
+
+class ChainComponent:
+    def __init__(self, chains):
+        self.possible_chains = chains
+
+    def find_fit(
+        self, substr: List[str], table: str
+    ) -> Optional[Tuple[Dict[str, str], List[str]]]:
+        to_find = substr[0]
+        if (
+            to_find in self.possible_chains
+            and table in self.possible_chains[to_find]["tables"]
+        ):
+            return self.possible_chains[to_find], substr[1:]
+        else:
+            return None
 
 
 class Rule:
-    def __init__(self, table: Table, chain: Chain):
+    def __init__(
+        self,
+        raw_form: str,
+        signatures: List[
+            List[Union[SignatureComponent, ChainComponent, RuleSpecification]]
+        ],
+        table: Optional[str],
+        chain: Optional[str],
+    ):
         self.table = table
         self.chain = chain
         self.components = []
-    
-    def __init__(self, raw_form):
-        parsed_form = self.parse_raw_form(raw_form)
-        
-    def parse_raw_form(self, raw_form) -> List[Dist[string, string]]:
-        pass
-        
+        if raw_form != "":
+            self.parsed_form = self.parse_raw_form(raw_form)
+
+    def parse_raw_form(self, raw_form, keep_best_estimate = False) -> Optional[List[Dict[str, str]]]:
+        for signature in signatures:
+            substr = raw_form.split(" ")
+            components = []
+            while i < len(signature) and substr:
+                part = signature[i]
+                result = ()
+                if type(part) == ChainComponent:
+                    result = part.find_fit(substr, self.table)
+                else:
+                    result = part.find_fit(substr)
+                if result:
+                    if type(part) == TableComponent and self.table == "":
+                        self.table == result["value"]
+                    components.append(result[0])
+                    substr = result[1]
+            if i < len(signature) or substr:
+                return components
+        return None
+
     def check_total_correctness(self) -> bool:
-        pass
+        raw_parts = [component["str_form"] for component in self.components]
+        actual_raw_form = " ".join(raw_parts)
+        return self.parse_raw_form(actual_raw_form) != None
 
     def check_partial_correctness(self) -> bool:
         pass
-    
-    def delete_element(self,id) -> bool:
-        pass
-    
+
+    def delete_element(self, id: int) -> bool:
+        if len(self.components) > id:
+            del self.components[id]
+            return True
+        return False
+
     def get_possible_elements(self) -> List:
         pass
 
-    def set_value(self) -> bool:
-        pass
-    
+    def set_value(self, id: int, value) -> bool:
+        if len(self.components) > id and "value" in self.components[id]:
+            self.components[id]["value"] = value
+            return True
+        return False
+
     def get_elements(self):
-        pass
-    
+        return self.components
+
+
+signatures = [
+    [
+        StartComponent(parser_entries.start_strs),
+        TableComponent(parser_entries.possible_tables),
+        CommandComponent(parser_entries.possible_commands),
+        RuleSpecification(None),
+    ]
+]
