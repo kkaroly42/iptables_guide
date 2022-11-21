91503f0eb319bceb5158e1a9a24e51ea99e263bb
Kecskeméti Károly <kecskemeti.karoly42@gmail.com>
2022-11-21 04:53:03 +0100

    Prepare for integration

--- IPTables_Guide/model/rule_system.py
+++ IPTables_Guide/model/rule_system.py
@@ -1,8 +1,8 @@
 from enum import Enum
 from typing import List, Dict, Optional
 
-from IPTables_Guide.model.packets import Packet
-from IPTables_Guide.model.rule_generator import Rule
+from IPTables_Guide.model.packets import *
+from IPTables_Guide.model.rule_generator import *
 
 class Table(Enum):
     FILTER = "filter"
